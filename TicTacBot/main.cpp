c4385d89c0947974d06a7682d37df16e55109537
Administrator <administrator@administrators-imac.local>
2018-09-19 14:06:39 -0700

    Initial Commit

---
+++ TicTacBot/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TicTacBot
+//
+//  Created by Administrator on 9/19/18.
+//  Copyright © 2018 Allen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
