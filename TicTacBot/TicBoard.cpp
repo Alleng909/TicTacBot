fa69db518b5e76dc882291c8ece534b9fa858f88
Administrator <administrator@administrators-imac.local>
2018-09-21 16:08:13 -0700

    Definitely something wrong with everything.

--- TicTacBot/TicBoard.cpp
+++ TicTacBot/TicBoard.cpp
@@ -16,19 +16,19 @@ TicBoard::TicBoard() {
         board[i] = 0;
     }
 }
-bool TicBoard::checkWin() {
+bool TicBoard::checkWin() { //Will probably be more efficient in the future if I keep track of last played and just check possible wins that involve that location.
     for(int i = 0; i < 3; i++) {
         //Checking vertical wins.
-        if (board[i] == board[i+3] && board[i] == board[i+6]) {
+        if (board[i] == board[i+3] && board[i] == board[i+6] && board[i] != 0) {
             return true;
         } //Checking horizontal wins.
-        else if (board[3*i] == board[3*i+1] && board[3*i] == board[3*i+2]) {
+        else if (board[3*i] == board[3*i+1] && board[3*i] == board[3*i+2] && board[3*i] != 0) {
             return true;
         } //Checking \ diagonal
-        else if (board[0] == board [4] && board[0] == board[8]) {
+        else if (board[0] == board [4] && board[0] == board[8] && board[0] != 0) {
             return true;
         } //Checking / diagonal
-        else if (board[2] == board[4] && board [2] == board[6]) {
+        else if (board[2] == board[4] && board [2] == board[6] && board[2] != 0) {
             return true;
         }
     }
@@ -36,7 +36,9 @@ bool TicBoard::checkWin() {
 }
 std::string TicBoard::getBoardString() {
     std::string returnMe;
-    
+    for(int i = 0; i < 9; i++) {
+        returnMe+= board[i];
+    }
     
     
     return returnMe;
@@ -60,6 +62,7 @@ bool TicBoard::pickSpot(int i) {
         return false;
     }
     board[i]+= (turn%2)*2-1; //Means Turn 1 will be marked with a 1, turn 2 with -1, etc...
+    turn++;
     return true;
 }
 
--- TicTacBot/main.cpp
+++ TicTacBot/main.cpp
@@ -14,7 +14,19 @@
 int main(int argc, const char * argv[]) {
     std::cout << "Starting Game!\n";
     
-    TicBoard() 
+    TicBoard* game = new TicBoard();
+    
+    //Quick test of checkwin function.
+    game->printBoard();
+    game->pickSpot(0);
+    std::cout << game->checkWin() << " should be " << false << std::endl;
+    game->pickSpot(1);
+    game->pickSpot(3);
+    game->pickSpot(4);
+    game->pickSpot(5);
+    game->pickSpot(6);
+    game->printBoard();
+    std::cout << game->checkWin() << " should be " << true << std::endl;
     
     return 0;
 }
